918f113cc2cb23adb9df23ecccbb1caa5372dc6f
islom <iabdugofurov99@gmail.com>
2025-01-17 05:17:55 +0500

    swagger togirlash

--- startup/swagger.js
+++ startup/swagger.js
@@ -18,128 +18,128 @@ const swaggerOptions = {
                 },
             },
             schemas: {
-                Statistics: {
-                    type: 'object',
-                    required: [
-                        'young',
-                        'middleAge',
-                        'oldAge',
-                        'nightVision',
-                        'onFoot',
-                        'bus',
-                        'auto',
-                        'bike',
-                        'otherTransport',
-                        'workingDayMonth',
-                        'offDayMonth',
-                        'workingDay',
-                        'dayOff',
-                        'monthViewsSeconds',
-                        'price',
-                        'locationId',
-                        'month'
-                    ],
-                    properties: {
-                        young: {
-                            type: 'number',
-                            description: 'Statistics for young age group.'
-                        },
-                        middleAge: {
-                            type: 'number',
-                            description: 'Statistics for middle-aged group.'
-                        },
-                        oldAge: {
-                            type: 'number',
-                            description: 'Statistics for old age group.'
-                        },
-                        nightVision: {
-                            type: 'number',
-                            description: 'Statistics for night vision conditions.'
-                        },
-                        onFoot: {
-                            type: 'number',
-                            description: 'Statistics for people on foot.'
-                        },
-                        bus: {
-                            type: 'number',
-                            description: 'Statistics for people using buses.'
-                        },
-                        auto: {
-                            type: 'number',
-                            description: 'Statistics for people using cars.'
-                        },
-                        bike: {
-                            type: 'number',
-                            description: 'Statistics for people using bikes.'
-                        },
-                        otherTransport: {
-                            type: 'number',
-                            description: 'Statistics for people using other types of transport.'
-                        },
-                        workingDayMonth: {
-                            type: 'number',
-                            description: 'Working days in 1 month'
-                        },
-                        offDayMonth: {
-                            type: 'number',
-                            description: '1 day off per month'
-                        },
-                        workingDayStatistics: {
-                            type: 'array',
-                            description: 'Statistics for working days.',
-                            items: {
-                                type: 'object',
-                                properties: {
-                                    hour: {
-                                        type: 'number',
-                                        description: 'Hour of the day.'
-                                    },
-                                    viewsNumber: {
-                                        type: 'number',
-                                        description: 'Number of views for the specific hour.'
-                                    }
-                                },
-                                required: ['hour', 'viewsNumber']
-                            }
-                        },
-                        dayOffStatistics: {
-                            type: 'array',
-                            description: 'Statistics for days off.',
-                            items: {
-                                type: 'object',
-                                properties: {
-                                    hour: {
-                                        type: 'number',
-                                        description: 'Hour of the day.'
-                                    },
-                                    viewsNumber: {
-                                        type: 'number',
-                                        description: 'Number of views for the specific hour.'
-                                    }
-                                },
-                                required: ['hour', 'viewsNumber']
-                            }
-                        },
-                        monthViewsSeconds: {
-                            type: 'number',
-                            description: 'Total number of views seconds for the month.'
-                        },
-                        price: {
-                            type: 'number',
-                            description: 'The price associated with the statistics.'
-                        },
-                        locationId: {
-                            type: 'number',
-                            format: 'objectId',
-                            description: 'Reference to the location associated with the statistics.'
-                        },
-                        month: {
-                            type: 'string',
-                            description: 'Add month'
-                        },
-                    }
-                }
-                ,
+                // Statistics: {
+                //     type: 'object',
+                //     required: [
+                //         'young',
+                //         'middleAge',
+                //         'oldAge',
+                //         'nightVision',
+                //         'onFoot',
+                //         'bus',
+                //         'auto',
+                //         'bike',
+                //         'otherTransport',
+                //         'workingDayMonth',
+                //         'offDayMonth',
+                //         'workingDay',
+                //         'dayOff',
+                //         'monthViewsSeconds',
+                //         'price',
+                //         'locationId',
+                //         'month'
+                //     ],
+                //     properties: {
+                //         young: {
+                //             type: 'number',
+                //             description: 'Statistics for young age group.'
+                //         },
+                //         middleAge: {
+                //             type: 'number',
+                //             description: 'Statistics for middle-aged group.'
+                //         },
+                //         oldAge: {
+                //             type: 'number',
+                //             description: 'Statistics for old age group.'
+                //         },
+                //         nightVision: {
+                //             type: 'number',
+                //             description: 'Statistics for night vision conditions.'
+                //         },
+                //         onFoot: {
+                //             type: 'number',
+                //             description: 'Statistics for people on foot.'
+                //         },
+                //         bus: {
+                //             type: 'number',
+                //             description: 'Statistics for people using buses.'
+                //         },
+                //         auto: {
+                //             type: 'number',
+                //             description: 'Statistics for people using cars.'
+                //         },
+                //         bike: {
+                //             type: 'number',
+                //             description: 'Statistics for people using bikes.'
+                //         },
+                //         otherTransport: {
+                //             type: 'number',
+                //             description: 'Statistics for people using other types of transport.'
+                //         },
+                //         workingDayMonth: {
+                //             type: 'number',
+                //             description: 'Working days in 1 month'
+                //         },
+                //         offDayMonth: {
+                //             type: 'number',
+                //             description: '1 day off per month'
+                //         },
+                //         workingDayStatistics: {
+                //             type: 'array',
+                //             description: 'Statistics for working days.',
+                //             items: {
+                //                 type: 'object',
+                //                 properties: {
+                //                     hour: {
+                //                         type: 'number',
+                //                         description: 'Hour of the day.'
+                //                     },
+                //                     viewsNumber: {
+                //                         type: 'number',
+                //                         description: 'Number of views for the specific hour.'
+                //                     }
+                //                 },
+                //                 required: ['hour', 'viewsNumber']
+                //             }
+                //         },
+                //         dayOffStatistics: {
+                //             type: 'array',
+                //             description: 'Statistics for days off.',
+                //             items: {
+                //                 type: 'object',
+                //                 properties: {
+                //                     hour: {
+                //                         type: 'number',
+                //                         description: 'Hour of the day.'
+                //                     },
+                //                     viewsNumber: {
+                //                         type: 'number',
+                //                         description: 'Number of views for the specific hour.'
+                //                     }
+                //                 },
+                //                 required: ['hour', 'viewsNumber']
+                //             }
+                //         },
+                //         monthViewsSeconds: {
+                //             type: 'number',
+                //             description: 'Total number of views seconds for the month.'
+                //         },
+                //         price: {
+                //             type: 'number',
+                //             description: 'The price associated with the statistics.'
+                //         },
+                //         locationId: {
+                //             type: 'number',
+                //             format: 'objectId',
+                //             description: 'Reference to the location associated with the statistics.'
+                //         },
+                //         month: {
+                //             type: 'string',
+                //             description: 'Add month'
+                //         },
+                //     }
+                // }
+                // ,
                 Location: {
                     type: 'object',
                     required: ['address', 'image', 'locationImage','video','screenPixel','fromHour','toHour','passportID','region'],
