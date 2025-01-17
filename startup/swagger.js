cd1f342cde6a496c66f39e1c108ac5c3328139d7
islom <iabdugofurov99@gmail.com>
2025-01-17 05:19:53 +0500

    swagger togirlash

--- startup/swagger.js
+++ startup/swagger.js
@@ -18,131 +18,10 @@ const swaggerOptions = {
                 },
             },
             schemas: {
-                // Statistics: {
-                //     type: 'object',
-                //     required: [
-                //         'young',
-                //         'middleAge',
-                //         'oldAge',
-                //         'nightVision',
-                //         'onFoot',
-                //         'bus',
-                //         'auto',
-                //         'bike',
-                //         'otherTransport',
-                //         'workingDayMonth',
-                //         'offDayMonth',
-                //         'workingDay',
-                //         'dayOff',
-                //         'monthViewsSeconds',
-                //         'price',
-                //         'locationId',
-                //         'month'
-                //     ],
-                //     properties: {
-                //         young: {
-                //             type: 'number',
-                //             description: 'Statistics for young age group.'
-                //         },
-                //         middleAge: {
-                //             type: 'number',
-                //             description: 'Statistics for middle-aged group.'
-                //         },
-                //         oldAge: {
-                //             type: 'number',
-                //             description: 'Statistics for old age group.'
-                //         },
-                //         nightVision: {
-                //             type: 'number',
-                //             description: 'Statistics for night vision conditions.'
-                //         },
-                //         onFoot: {
-                //             type: 'number',
-                //             description: 'Statistics for people on foot.'
-                //         },
-                //         bus: {
-                //             type: 'number',
-                //             description: 'Statistics for people using buses.'
-                //         },
-                //         auto: {
-                //             type: 'number',
-                //             description: 'Statistics for people using cars.'
-                //         },
-                //         bike: {
-                //             type: 'number',
-                //             description: 'Statistics for people using bikes.'
-                //         },
-                //         otherTransport: {
-                //             type: 'number',
-                //             description: 'Statistics for people using other types of transport.'
-                //         },
-                //         workingDayMonth: {
-                //             type: 'number',
-                //             description: 'Working days in 1 month'
-                //         },
-                //         offDayMonth: {
-                //             type: 'number',
-                //             description: '1 day off per month'
-                //         },
-                //         workingDayStatistics: {
-                //             type: 'array',
-                //             description: 'Statistics for working days.',
-                //             items: {
-                //                 type: 'object',
-                //                 properties: {
-                //                     hour: {
-                //                         type: 'number',
-                //                         description: 'Hour of the day.'
-                //                     },
-                //                     viewsNumber: {
-                //                         type: 'number',
-                //                         description: 'Number of views for the specific hour.'
-                //                     }
-                //                 },
-                //                 required: ['hour', 'viewsNumber']
-                //             }
-                //         },
-                //         dayOffStatistics: {
-                //             type: 'array',
-                //             description: 'Statistics for days off.',
-                //             items: {
-                //                 type: 'object',
-                //                 properties: {
-                //                     hour: {
-                //                         type: 'number',
-                //                         description: 'Hour of the day.'
-                //                     },
-                //                     viewsNumber: {
-                //                         type: 'number',
-                //                         description: 'Number of views for the specific hour.'
-                //                     }
-                //                 },
-                //                 required: ['hour', 'viewsNumber']
-                //             }
-                //         },
-                //         monthViewsSeconds: {
-                //             type: 'number',
-                //             description: 'Total number of views seconds for the month.'
-                //         },
-                //         price: {
-                //             type: 'number',
-                //             description: 'The price associated with the statistics.'
-                //         },
-                //         locationId: {
-                //             type: 'number',
-                //             format: 'objectId',
-                //             description: 'Reference to the location associated with the statistics.'
-                //         },
-                //         month: {
-                //             type: 'string',
-                //             description: 'Add month'
-                //         },
-                //     }
-                // }
-                // ,
+
                 Location: {
                     type: 'object',
-                    required: ['address', 'image', 'locationImage','video','screenPixel','fromHour','toHour','passportID','region'],
+                    required: ['address', 'image', 'locationImage','video','screenPixel','fromHour','toHour','passportID'],
                     properties: {
                         address: {type: 'string', description: 'Name of the user'},
                         image: {type: 'string', description: 'Login of the user'},
@@ -152,18 +31,16 @@ const swaggerOptions = {
                         fromHour: {type: 'string', description: 'Password for the user account'},
                         toHour: {type: 'string', description: 'Password for the user account'},
                         passportID: {type: 'string', description: 'Password for the user account'},
-                        region: {type: 'string', description: 'Password for the user account'},
                     },
                 },
 
                 User: {
                     type: 'object',
-                    required: ['name', 'login', 'password','role'],
+                    required: ['name', 'login', 'password'],
                     properties: {
                         name: {type: 'string', description: 'Name of the user'},
                         login: {type: 'string', description: 'Login of the user'},
                         password: {type: 'string', description: 'Password for the user account'},
-                        role: {type: 'string', description: 'Password for the user account'},
                     },
                 },
 
